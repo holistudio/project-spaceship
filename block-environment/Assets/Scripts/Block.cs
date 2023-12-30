d1c65c13882ede83960c8e917436cc6e0c99311f
holistudio <daniel.bin.lu@gmail.com>
2023-12-29 22:02:34 -0500

    Update CheckModified() for Block class

--- block-environment/Assets/Scripts/Block.cs
+++ block-environment/Assets/Scripts/Block.cs
@@ -14,20 +14,6 @@ public class Block : MonoBehaviour
 
     private bool modified = false;
 
-    void UpdateProperties()
-    {
-        type = gameObject.name;
-        position = transform.position;
-        orientation = GetOrientation();
-        Color rgba_color = gameObject.GetComponent<MeshRenderer>().material.color;
-        color = new Vector3(rgba_color.r,rgba_color.g,rgba_color.b); 
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        UpdateProperties();
-    }
-
     int GetOrientation()
     {
         int angle = (int) Math.Round(transform.rotation.eulerAngles.y);
@@ -46,9 +32,26 @@ public class Block : MonoBehaviour
         return -1;
     }
 
+    void UpdateProperties()
+    {
+        type = gameObject.name;
+        position = transform.position;
+        orientation = GetOrientation();
+        Color rgba_color = gameObject.GetComponent<MeshRenderer>().material.color;
+        color = new Vector3(rgba_color.r,rgba_color.g,rgba_color.b); 
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateProperties();
+    }
+
     bool CheckModified()
     {
-        if((transform.position == position) && (GetOrientation() == orientation))
+        Color rgba_color = gameObject.GetComponent<MeshRenderer>().material.color;
+        Vector3 latest_color = new Vector3(rgba_color.r,rgba_color.g,rgba_color.b);
+        if((transform.position == position) && (GetOrientation() == orientation) && (latest_color == color))
         {
             return false;
         }
