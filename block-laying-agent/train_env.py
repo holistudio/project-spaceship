661c363e4b7b9e446e2f2ea46939a7bc5bafb460
holistudio <daniel.bin.lu@gmail.com>
2024-03-09 18:10:09 -0500

    no_block_conflict function for environment

--- block-laying-agent/train_env.py
+++ block-laying-agent/train_env.py
@@ -93,7 +93,7 @@ while (x<max_x):
 NUM_X, NUM_Y, NUM_Z = target_vox_tensor.shape
 
 grid_sizes = (NUM_X, NUM_Y, NUM_Z)
-occupied_tensor = torch.zeros((NUM_X,NUM_Y,NUM_Z), dtype=torch.long)
+grid_tensor = torch.zeros((NUM_X,NUM_Y,NUM_Z), dtype=torch.long) # just tracks which cells are occupied
 
 def reset():
     # Initialize current_design_tensor
@@ -106,14 +106,42 @@ def reset():
     return current_design_tensor
 
 def no_block_conflict(actions):
+    check_cells = actions['occupied_cells']
+    n_cells, _ = check_cells.shape
+    for i in range(n_cells):
+        x,y,z = list(check_cells[i])
+        if grid_tensor[x,y,z] == 1:
+            return False
     return True
 
 def step(state):
     agent = Agent.CNNAgent(grid_sizes=grid_sizes, num_orient=NUM_ORIENTATION, block_info_size=BLOCK_INFO, block_types=BLOCK_TYPES)
 
-    actions = agent.select_actions(state)
+    conflict_check_passed = False
+    while (not conflict_check_passed):
+        agent_actions = agent.select_actions(state)
 
-    print(actions)
+        block_type_i, orientation, grid_x, grid_y, grid_z = agent_actions
+        block_type = list(BLOCK_DEFINITIONS.keys())[block_type_i]
+
+        if orientation == 0:
+            grid_position = np.array([grid_x,grid_y,grid_z])
+            occupied_cells = grid_position + BLOCK_DEFINITIONS[block_type]['o0_cells']
+        if orientation == 1:
+            grid_position = np.array([grid_x,grid_y,grid_z])
+            occupied_cells = grid_position + BLOCK_DEFINITIONS[block_type]['o1_cells']
+
+        actions = {
+            "block_type": block_type,
+            "grid_x": grid_x,
+            "grid_y": grid_y,
+            "grid_z": grid_z,
+            "orientation": orientation,
+            "occupied_cells": occupied_cells
+        }
+        conflict_check_passed = no_block_conflict(actions)
+
+    print("Passed")
 
 if __name__ == "__main__":
     current_design_tensor = reset()
