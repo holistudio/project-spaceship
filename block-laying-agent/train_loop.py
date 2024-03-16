0864e7a4074ffdfcc507f5236904f6f79c6a7b82
holistudio <daniel.bin.lu@gmail.com>
2024-03-15 21:39:57 -0400

    add logging to train_loop

--- block-laying-agent/train_loop.py
+++ block-laying-agent/train_loop.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 import numpy as np
 import HCNN_agent as Agent
 import training_env as Env
@@ -9,6 +12,20 @@ agent = Agent.CNNAgent(grid_sizes=env.grid_sizes,
                        num_orient=env.num_orient, 
                        block_info_size=env.block_info_size, 
                        block_types=env.block_types)
+log = {
+    "record":[]
+}
+
+
+def log_everything(e, block, agent_log, reward, terminal):
+    event = {
+        "episode": int(e),
+        "block": int(block),
+        "agent": agent_log,
+        "reward": float(reward),
+        "terminal": bool(terminal)
+    }
+    log["record"].append(event)
 
 for ep in range(NUM_EPISODES):
     print(f'==EPISODE {ep}==')
@@ -25,6 +42,7 @@ for ep in range(NUM_EPISODES):
         # print('AGENT updates EXP')
         agent.update_experience(state,agent_actions,next_state,reward,terminal)
 
+        log_everything(ep, env.block_seq_index, agent.log, reward, terminal)
         state = next_state
     
     print(f'==END EPISODE {ep}==')
@@ -32,3 +50,16 @@ for ep in range(NUM_EPISODES):
     print()
 
     agent.update_policy(ep+1)
+
+    log_file = os.path.join('results',f'episode_{ep}_log.json')
+
+    with open(log_file, 'w') as f:
+        print("Saving log...")
+        print()
+        print()
+        json.dump(log, f)
+
+    # Clear log
+    log = {
+        "record":[]
+    }
