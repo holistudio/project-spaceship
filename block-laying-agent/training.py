6ee7d389ea925cb482d6caf9ccad435a4d99021d
holistudio <daniel.bin.lu@gmail.com>
2025-11-14 19:49:13 -0500

    Update training.py

--- block-laying-agent/training.py
+++ block-laying-agent/training.py
@@ -3,33 +3,23 @@ import json
 import copy
 import datetime
 
-import HCNN_agent as Agent
-import env4training as Env
+from environment import BlockEnvironment
+from teacher_agent import TeacherAgent
 
 NUM_EPISODES = 2
-DIR = os.path.join('results', 'HCNN')
+DIR = os.path.join('results', 'teacher')
 
 START_EP = 0
-LOAD_CHECKPOINT = False
-
-env = Env.BlockTrainingEnvironment()
-agent = Agent.CNNAgent(grid_sizes=env.grid_sizes, 
-                       num_orient=env.num_orient, 
-                       block_info_size=env.block_info_size, 
-                       block_types=env.block_types)
 
 log = {
     "record":[]
 }
 
-
-def log_everything(e, block, env_log, agent_log, loss, reward, terminal):
+def log_everything(e, block, env_log, reward, terminal):
     event = {
         "episode": int(e),
         "block": int(block),
         "env": copy.deepcopy(env_log),
-        "agent": copy.deepcopy(agent_log),
-        "loss": float(loss),
         "reward": float(reward),
         "terminal": bool(terminal)
     }
@@ -37,59 +27,84 @@ def log_everything(e, block, env_log, agent_log, loss, reward, terminal):
 
 check_every = 50
 
-if LOAD_CHECKPOINT:
-    agent.load_checkpoint()
-    START_EP = agent.episode + 1
+env = BlockEnvironment()
+
+
+terminal = False
+a_i = 0
 
 start_time = datetime.datetime.now()
 
-for ep in range(NUM_EPISODES):
-    episode = START_EP + ep
-    start_block = 0
-    check_block = check_every
-    print(f'===== EPISODE {episode} =====')
+episode = START_EP
+start_block = 0
+check_block = check_every
 
-    state, reward, terminal = env.reset()
+print(f'===== EPISODE {episode} =====')
+
+env.reset()
+agent0 = TeacherAgent(env.target_tensor)
+agent1 = TeacherAgent(env.target_tensor)
+agents = [agent0, agent1]
+
+print('== PLACING BLOCKS ==')
+while not terminal:
+    agent = agents[a_i]
+
+    observation, reward, terminal, info = env.last()
+
+    if terminal:
+        next_observation = None
+    else:
+        action_mask = info['action_mask']
+        action = agent.step(observation, action_mask)
+        next_observation = env.step(action)
+
+    # agent1.record(observation, action, next_observation, reward)
+
+    if a_i < len(agents)-1:
+        a_i += 1
+    else:
+        a_i = 0
+
+    log_everything(episode, env.block_ix-1, env.env_log, reward, terminal)
+    if ((env.block_ix+1) >= check_block) or terminal:
+        print(f'{datetime.datetime.now()} TRAINING: Block {env.block_ix}, Reward = {reward:.2f}, Percent complete = {env.perc_complete*100:.2f}%')
+        log_file = os.path.join(DIR,f'episode_{episode}_blocks_{start_block}-{env.block_ix}_log.json')
+        start_block = env.block_ix+1
+        check_block += check_every
+
+        with open(log_file, 'w') as f:
+            json.dump(log, f)
+
+        # Clear log
+        log = {
+            "record":[]
+        }
+
+
+# NUM_ATTEMPTS = 10
+
+# while not terminal:
     
-    print('== PLACING BLOCKS ==')
-    while not terminal:
-        # print('= AGENT selects ACTION =')
-        agent_actions, env_actions = agent.select_actions(state)
+    # observation, reward, terminal, info = env.last()
 
-        # print('= ENV makes STEP =')
-        next_state, reward, terminal = env.step(env_actions)
+    # for attempt in range(NUM_ATTEMPTS)
+        # attempt_action = agent.step(observation)
+        # valid = env.check_action(attempt_action)
+        # if valid: break
 
-        # print('= AGENT updates EXP =')
-        loss = agent.update_experience(state, agent_actions, next_state, reward, terminal)
+    # if valid:
+        # action = attempt_action
+    # else:
+        # maybe let the environment add another valid block 
+        # but then have the agent record that action and 
+        # next state as its experience
 
-        log_everything(episode, env.block_seq_index, env.log, agent.log, loss, reward, terminal)
+    # env.step(action)
 
-        # print(env.block_seq_index)
-        if ((env.block_seq_index+1) >= check_block) or terminal:
-            print(f'{datetime.datetime.now()} TRAINING: Block {env.block_seq_index}, Reward = {reward:.2f}, Percent complete = {env.perc_complete*100:.2f}%')
-            # print('!!!!SAVE!!!!')
-            agent.save_checkpoint(loss)
-            log_file = os.path.join(DIR,f'episode_{episode}_blocks_{start_block}-{env.block_seq_index}_log.json')
-            start_block = env.block_seq_index+1
-            check_block += check_every
+    # if terminal:
+        # next_observation = None
+    # else:
+        # next_observation = env.step(action)
 
-            with open(log_file, 'w') as f:
-                # print("Saving log...")
-                # print()
-                # print()
-                json.dump(log, f)
-
-            # Clear log
-            log = {
-                "record":[]
-            }
-            
-        state = next_state
-    
-    print()
-    print(f'TRAINING TIME ELAPSED: {datetime.datetime.now() - start_time}')
-    print(f'===== END EPISODE {episode} =====')
-    print()
-    print()
-
-    agent.update_policy(episode+1)   
+    # agent.record(observation, action, next_observation, reward)
