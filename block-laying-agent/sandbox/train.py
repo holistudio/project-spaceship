131bfb16a8a06edd5d3c92d47b982ea6cd4245f1
holistudio <daniel.bin.lu@gmail.com>
2026-05-17 19:32:03 -0400

    Update train.py
    testing with two "random agents" using env.sample_action(mask)

--- block-laying-agent/sandbox/train.py
+++ block-laying-agent/sandbox/train.py
@@ -9,39 +9,42 @@ def main():
     # teacher_agent = TeacherAgent()
     # block_agent = PPOBlockAgent()
 
+    agent_list = ['random', 'random']
     # agent_list = [teacher_agent, block_agent]
     # agent_list = [teacher_agent, 'random']
     # agent_list = [user, block_agent]
 
     for ep in range(episodes):
         print(f"\n\n{"#"*5} EPISODE {ep+1:,} {"#"*5}\n")
-        idx = 0
-        env.reset()
+        
+        idx = 0 # for tracking whose turn it is on agent_list
 
+        env.reset()
         while not (env.termination or env.truncation):
             print(env.timestep, env.current_player)
-            # agent = agent_list[idx]
+            agent = agent_list[idx]
             observation, reward, termination, truncation = env.last()
             
-            # if termination or truncation:
-                # actions = None
-            # else:
-                # mask = observation["actions_mask"]
-                # if agent == 'random':
-                    # actions = env.actions_space.sample(mask)
-                # else:
-                    # actions = agent.step(observation)
+            if termination or truncation:
+                actions = None
+            else:
+                mask = observation["action_mask"]
+                if agent == 'random':
+                    action = env.sample_action(mask)
+                    actions = {"new_blocks": action}
+                else:
+                    actions = agent.step(observation)
             
             env.step()
             # env.step(actions)
 
-            # if idx % 2 != 0:
+            # if env.current_player == 'agent':
                 # TODO: revisit this later in terms of 
                 # associating the reward correctly to 
                 # block_agent's PREVIOUS (NOT LATEST) obs/actions
                 # agent.experience(observation, actions, reward)
                 
-            # idx = (idx+1)%2
+            idx = (idx+1)%2
     # env.close()
     pass
 
